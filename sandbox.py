1148d240cd2e53e0a1b24957c3cdda52e365defb
«Sergei» <isakov94@gmail.com>
2020-06-16 10:40:50 +1000

    Добавил заполнение словаря с ответами

--- sandbox.py
+++ sandbox.py
@@ -1,31 +1,34 @@
 import random 
-from datetime import datetime, timedelta
+from datetime import date, time, datetime, timedelta
 
 b = {'DS100': '250',
      'DS400': '400',
      'DH60': '200',
      'DH250': '350'}
 
+start_time = datetime.now()
+result = {} # в этот словарь записываются пары вопрос:правильно или вопрос:неправильно
+
 def get_pair():
     pair = random.choice(list(b.items()))
     question = pair[0]
     tru_answer = pair[1]
     answer = input(f'Напиши цену {question} ')
-    result = {} # в этот словарь предполагается записывать пары вопрос:правильно или вопрос:неправильно
-    start_time = datetime.now()
     if answer == tru_answer:
         print('правильно')
-        result[answer] = 'правильно'
+        result[f'{question}'] = 'правильно'
         get_pair()
 
     elif answer == 'стоп':
         finish_time = datetime.now()
         delta_time = finish_time - start_time
-        print(f"Баста! Ты тренировался c {start_time} по {finish_time}")
+        #str_delta_time = str(delta_time) не знаю, как перевести delta_time в читаемый вид
+        #user_test_time = time.strptime(str_delta_time, '%H:%M:%S.%f')
+        print(f"Баста! Ты тренировался {delta_time}.\nРезультаты вот:\n{result}")
 
     else:
         print('неправильно')
-        result[answer] = 'неправильно'
+        result[f'{question}'] = 'неправильно'
         get_pair()
         
         
--- sandbox2.py
+++ sandbox2.py
@@ -1,7 +1,4 @@
-from datetime import date, time, datetime, timedelta
-date = date(2020, 6, 7)
-print(date)
-
-strdatetime = '01/01/17 12:10:03.234567'
-final_time = datetime.strptime(strdatetime, '%d/%m/%y %H:%M:%S.%f')
-print(final_time)
+answer = 'any_text'
+result = {}
+result[f'{answer}'] = 'value'
+print(result)
